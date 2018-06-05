a64a5291ade7f2aa96620fb1b7bab5499e7b33eb
Marchi <58844411@qq.com>
2018-06-05 22:50:39 +0800

    code update

---
+++ eureka-feign-client/src/main/java/com/example/eurekafeignclient/feign/EurekaClientFeign.java
@@ -0,0 +1,13 @@
+package com.example.eurekafeignclient.feign;
+
+import com.example.eurekafeignclient.config.FeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eureka-client",configuration = FeignConfig.class)
+public interface EurekaClientFeign {
+
+    @GetMapping("/hi")
+    String sayHiFromClientEureka(@RequestParam(value="name") String name);
+}
---
+++ eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/controller/RibbonController.java
@@ -0,0 +1,18 @@
+package com.example.eurekaribbonclient.controller;
+
+import com.example.eurekaribbonclient.service.RibbonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+    @Autowired
+    RibbonService ribbonService;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(required = false,defaultValue = "forezp") String name){
+        return ribbonService.hi(name);
+    }
+}
